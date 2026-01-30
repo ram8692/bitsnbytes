5332cc000d7f8e643dfa29dfc9fe800c5e0628f3
Rambabu <ramgupta9137@gmail.com>
2026-01-30 18:31:19 +0530

    component tutorial

---
+++ productlist/src/main/java/com/bitsnbytes/productlist/MyComponent.java
@@ -0,0 +1,16 @@
+package com.bitsnbytes.productlist;
+
+import org.springframework.stereotype.Component;
+
+
+public class MyComponent {
+
+    MyComponent() {
+        System.out.println("hi from MyComponent");
+    }
+
+    //write one function
+    public void getMessage() {
+        System.out.println("hi from getMessage");
+    }
+}
